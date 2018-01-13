cb5264d21960b5f204f25b75c6a76ea18620d1c3
Keith Gregory <contact@kdgregory.com>
2018-01-13 13:59:43 -0500

    checkpoint: KinesisUtils.describeShards()

--- utils/src/main/java/com/kdgregory/aws/utils/kinesis/KinesisUtils.java
+++ utils/src/main/java/com/kdgregory/aws/utils/kinesis/KinesisUtils.java
@@ -14,6 +14,7 @@
 
 package com.kdgregory.aws.utils.kinesis;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -26,34 +27,69 @@ import com.amazonaws.services.kinesis.model.*;
 public class KinesisUtils
 {
     /**
+     *  Executes a <code>DescribeStream</code> request, retrying with delay if
+     *  the request fails due to throttling. Returns null if the stream doesn't
+     *  exist or could not be described within the timeout. Otherwise returns
+     *  the stream description (not the API result).
+     */
+    public static StreamDescription describeStream(AmazonKinesis client, DescribeStreamRequest request, long timeout)
+    {
+        try
+        {
+            return client.describeStream(request).getStreamDescription();
+        }
+        catch (ResourceNotFoundException ex)
+        {
+            System.out.println("caught!");
+            return null;
+        }
+    }
+
+
+    /**
      *  A wrapper around <code>DescribeStream</code> that retrieves all shard
      *  descriptions and retries if throttled. Returns null if the stream does
-     *  not exist.
-     *  
+     *  not exist or we could not retrieve all shards within the timeout.
+     *
      *  @param  client      The AWS client used to make requests.
+     *  @param  streamName  The stream to describe.
      *  @param  timeout     The total number of milliseconds to attempt retries. This
      *                      method will retry the operation every 100 milliseconds
      *                      until this timeout expires.
      */
-    public static List<Shard> describeShards(AmazonKinesis client, long timeout)
+    public static List<Shard> describeShards(AmazonKinesis client, String streamName, long timeout)
     {
-        throw new UnsupportedOperationException("FIXME - implement");
+        List<Shard> result = new ArrayList<Shard>();
+        String lastShardId = null;
+        do
+        {
+            DescribeStreamRequest request = new DescribeStreamRequest().withStreamName(streamName);
+            if (lastShardId != null) request.setExclusiveStartShardId(lastShardId);
+            StreamDescription description = describeStream(client, request, timeout);
+            if (description == null) return null;
+            List<Shard> shards = description.getShards();
+            result.addAll(shards);
+            lastShardId = description.getHasMoreShards()
+                        ? shards.get(shards.size() - 1).getShardId()
+                        : null;
+        } while (lastShardId != null);
+        return result;
     }
-    
-    
+
+
     /**
      *  Retrieves the named stream's status, retrying if rate-limited. Returns the
      *  status, null if the stream does not exist.
      */
-    public static String retrieveStreamStatus(AmazonKinesis client, String streamName)
+    public static String describeStreamStatus(AmazonKinesis client, String streamName)
     {
         throw new UnsupportedOperationException("FIXME - implement");
     }
-    
-    
+
+
     /**
      *  Creates a stream and waits for it to become available.
-     *  
+     *
      *  @param  client      The AWS client used to make requests.
      *  @param  streamName  The name of the stream.
      *  @param  numShards   The number of shards to create.
@@ -62,12 +98,12 @@ public class KinesisUtils
     {
         throw new UnsupportedOperationException("FIXME - implement");
     }
-    
-    
+
+
     /**
-     *  Creates a stream with non-standard retention period and waits for 
+     *  Creates a stream with non-standard retention period and waits for
      *  it to become available.
-     *  
+     *
      *  @param  client      The AWS client used to make requests.
      *  @param  streamName  The name of the stream.
      *  @param  numShards   The number of shards to create.
@@ -81,7 +117,7 @@ public class KinesisUtils
 
     /**
      *  Deletes a stream and waits for it to no longer be available.
-     *  
+     *
      *  @param  client      The AWS client used to make requests.
      *  @param  streamName  The name of the stream.
      */
@@ -89,8 +125,8 @@ public class KinesisUtils
     {
         throw new UnsupportedOperationException("FIXME - implement");
     }
-    
-    
+
+
     /**
      *  Retrieves shard iterators for the specified stream. The goal of this
      *  function is to allow uninterrupted reading of a stream based on saved
@@ -98,7 +134,7 @@ public class KinesisUtils
      *  <ul>
      *  <li> For any currently-open shard that is in the map, this function
      *       will return an <code>AFTER_SEQUENCE_NUMBER</code> iterator (this
-     *       is the normal case).  
+     *       is the normal case).
      *  <li> For any closed shard that is in the map, this function will return
      *       an <code>AFTER_SEQUENCE_NUMBER</code> iterator if-and-only-if the
      *       last sequence number in the shard's range is greater than that in
@@ -111,14 +147,14 @@ public class KinesisUtils
      *       function will return a <code>TRIM_HORIZON</code> iterator (so will
      *       identify new shards).
      *  </ul>
-     *  To start reading a stream from its beginning, supply an empty map. 
-     *  
+     *  To start reading a stream from its beginning, supply an empty map.
+     *
      *  @param  client      The AWS client used to make requests.
      *  @param  streamName  The name of the stream.
      *  @param  seqnums     A map associating shard ID with the sequence number
      *                      of the last record read from that shard.
-     *                      
-     *  @return A map associating shard ID with an iterator for that shard.                    
+     *
+     *  @return A map associating shard ID with an iterator for that shard.
      */
     public static Map<String,String> retrieveShardIterators(
         AmazonKinesis client, String streamName, Map<String,String> seqnums)
