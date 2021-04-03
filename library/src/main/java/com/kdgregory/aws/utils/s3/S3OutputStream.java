395ec96b10ac221bf22c8e1e40ef211343e17646
Keith Gregory <contact@kdgregory.com>
2021-04-03 09:48:41 -0400

    add S3OutputStream

---
+++ library/src/main/java/com/kdgregory/aws/utils/s3/S3OutputStream.java
@@ -0,0 +1,348 @@
+// Copyright (c) Keith D Gregory
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.kdgregory.aws.utils.s3;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.*;
+
+
+/**
+ *  Uses a multi-part upload to send arbitrary-length streams to S3. It is somewhat
+ *  simpler to use than <code>TransferManager</code>, and does not spin up extra
+ *  threads.
+ *  <p>
+ *  Use as with any other output stream, with one caveat: you should wrap all code
+ *  in a try/catch, to abort the stream on any exception. For example:
+ *  <p>
+ *  <pre>
+ *      InputStream in = // something
+ *      try (S3OutputStream out = new S3OutputStream(s3Client, "argle", "bargle"))
+ *      {
+ *          try
+ *          {
+ *              IOUtil.copy(in, out);
+ *          }
+ *          catch (Exception ex)
+ *          {
+ *              out.abort();
+ *              // rethrow or log exception
+ *          }
+ *      }
+ *  </pre>
+ *  <p>
+ *  Warning: if you don't call {@link #close} or {@link #abort}, you will be left
+ *  with an incomplete multi-part upload. This will incur monthly charges for S3
+ *  storage. To avoid such charges, you configure your bucket with a life-cycle
+ *  rule that deletes incomplete uploads.
+ *  <p>
+ *  Internally, it stores all writes in a buffer until it's accumulated enough
+ *  to upload a part (by default, 5 MB). At that point it uploads the part inline
+ *  (note that this will pause your application) and resets for the next part. On
+ *  close, it writes all outstanding data as a final part.
+ *  <p>
+ *  Implementation note: the actual upload starts when the buffer first fills. An
+ *  improperly-configured client will not be discovered until then.
+ */
+public class S3OutputStream
+extends OutputStream
+{
+    private Log logger = LogFactory.getLog(getClass());
+
+    // these are set by constructor
+    private AmazonS3 client;
+    private String bucket;
+    private String key;
+    private int partSize;
+    private ObjectMetadata metadata;
+
+    // this is assigned once the upload has started
+    private String uploadToken;
+
+    // this is assigned at construction, nulled at completion or if aboarted
+    private ExposedByteArrayOutputStream buffer;
+
+    private int partNumber;
+    private List<PartETag> partTags = new ArrayList<>();
+
+
+    /**
+     *  Constructs an instance with the default part size (5 MB) and default
+     *  object metadata.
+     *  <p>
+     *  Uploads using this constructor are limited to 50GB; if you need to
+     *  upload more data, you must specify a larger part size.
+     *
+     *  @param  client  A properly-configured client, used to perform the write.
+     *  @param  bucket  The bucket where the stream is to be written.
+     *  @param  key     The name of the file in that bucket.
+     */
+    public S3OutputStream(AmazonS3 client, String bucket, String key)
+    {
+        this(client, bucket, key, new ObjectMetadata(), 5 * 1024 * 1024);
+    }
+
+
+    /**
+     *  Constructs an instance with the default part size (5 MB) and provided
+     *  object metadata.
+     *  <p>
+     *  Uploads using this constructor are limited to 50GB; if you need to
+     *  upload more data, you must specify a larger part size.
+     *
+     *  @param  client  A properly-configured client, used to perform the write.
+     *  @param  bucket  The bucket where the stream is to be written.
+     *  @param  key     The name of the file in that bucket.
+     *  @param  metadata    Object metadata. This can be used to provide content
+     *                      type or caller-defined headers. Any provided content
+     *                      length is ignored.
+     */
+    public S3OutputStream(AmazonS3 client, String bucket, String key, ObjectMetadata metadata)
+    {
+        this(client, bucket, key, metadata, 5 * 1024 * 1024);
+    }
+
+
+    /**
+     *  Constructs an instance with the specified part size and explicit object
+     *  metadata. Part size must be at least 5 MB.
+     *
+     *  @param  client      A properly-configured client, used to perform the write.
+     * @param  bucket      The bucket where the stream is to be written.
+     * @param  key         The name of the file in that bucket.
+     * @param  metadata    Object metadata. This can be used to provide content
+     *                      type or caller-defined headers. Any provided content
+     *                      length is ignored.
+     * @param  partSize    Number of bytes to buffer before uploading.
+     */
+    public S3OutputStream(AmazonS3 client, String bucket, String key, ObjectMetadata metadata, int partSize)
+    {
+        this.client = client;
+        this.bucket = bucket;
+        this.key = key;
+        this.metadata = metadata;
+        this.partSize = partSize;
+        this.buffer = new ExposedByteArrayOutputStream(partSize);
+    }
+
+//----------------------------------------------------------------------------
+//  OutputStream implementation
+//----------------------------------------------------------------------------
+
+    /**
+     *  Closes the stream, writing the last segment and completing the upload.
+     *  No-op if the stream has been aborted.
+     */
+    @Override
+    public void close()
+    throws IOException
+    {
+        if (buffer == null)
+            return;
+
+        if (uploadToken == null)
+        {
+            logger.debug("buffer size is " + buffer.size() + "; using PutObject");
+            metadata.setContentLength(buffer.size());
+            client.putObject(bucket, key, buffer.asInputStream(), metadata);
+        }
+        else
+        {
+            writePart();
+            logger.debug("completing multi-part upload; token: " + uploadToken.substring(0, 16));
+            CompleteMultipartUploadRequest completeRequest = new CompleteMultipartUploadRequest(bucket, key, uploadToken, partTags);
+            client.completeMultipartUpload(completeRequest);
+        }
+
+        buffer = null;
+    }
+
+
+    /**
+     *  This has no effect; the underlying stream writes data as it reaches the
+     *  predefined buffer size.
+     */
+    @Override
+    public void flush()
+    throws IOException
+    {
+        // no-op
+    }
+
+
+    /**
+     *  Writes a single byte to the buffer. Will trigger an upload if the buffer
+     *  size exceeds configured part size.
+     */
+    @Override
+    public void write(int b)
+    throws IOException
+    {
+        if (buffer == null)
+            return;
+
+        buffer.write(b);
+        optWritePart();
+    }
+
+
+    /**
+     *  Writes an array to the buffer. Will trigger an upload if the buffer size exceeds
+     *  configured part size.
+     *  <p>
+     *  Warning: the buffer will be expanded if necessary to contain the entire array.
+     */
+    @Override
+    public void write(byte[] b)
+    throws IOException
+    {
+        if (buffer == null)
+            return;
+
+        buffer.write(b);
+        optWritePart();
+    }
+
+
+    /**
+     *  Writes a sub-array array to the buffer. Will trigger an upload if the buffer size
+     *  exceeds  configured part size.
+     *  <p>
+     *  Warning: the buffer will be expanded if necessary to contain the entire sub-array.
+     */
+    @Override
+    public void write(byte[] b, int off, int len)
+    throws IOException
+    {
+        if (buffer == null)
+            return;
+
+        buffer.write(b, off, len);
+        optWritePart();
+    }
+
+//----------------------------------------------------------------------------
+//  Other Public Methods
+//----------------------------------------------------------------------------
+
+    /**
+     *  Aborts the multi-part upload.
+     */
+    public void abort()
+    {
+        if (buffer == null)
+            return;
+
+        buffer = null;
+
+        if (uploadToken == null)
+            return;
+
+        logger.debug("aborting multi-part upload; token: " + uploadToken.substring(0, 16));
+        AbortMultipartUploadRequest request = new AbortMultipartUploadRequest(bucket, key, uploadToken);
+        client.abortMultipartUpload(request);
+    }
+
+//----------------------------------------------------------------------------
+//  Internals
+//----------------------------------------------------------------------------
+
+    /**
+     *  This subclass allows access to the actual buffer, so that we don't
+     *  need to duplicate it when writing to S3. Note that we allocate a bit
+     *  more than is needed, to avoid unnecessary expansion.
+     */
+    private static class ExposedByteArrayOutputStream
+    extends ByteArrayOutputStream
+    {
+        private int partSize;
+
+        public ExposedByteArrayOutputStream(int partSize)
+        {
+            super((int)(partSize * 1.2));
+            this.partSize = partSize;
+        }
+
+        public InputStream asInputStream()
+        {
+            int size = Math.min(partSize, count);
+            return new ByteArrayInputStream(buf, 0, size);
+        }
+
+        /**
+         *  Resets the stream to "remove" the bytes corresponding to an uploaded
+         *  part while preserving bytes written after that part.
+         *  <p>
+         *  Note: relies on ByteArrrayInputStream exposing its internals.
+         */
+        @Override
+        public void reset()
+        {
+            int preserveCount = count - partSize;
+            if (preserveCount <= 0)
+            {
+                super.reset();
+                return;
+            }
+
+            System.arraycopy(buf, partSize, buf, 0, preserveCount);
+            count = preserveCount;
+        }
+    }
+
+
+    private void optWritePart()
+    {
+        while (buffer.size() >= partSize)
+        {
+            if (uploadToken == null)
+            {
+                logger.debug("initiating multi-part upload");
+                InitiateMultipartUploadRequest initRequest = new InitiateMultipartUploadRequest(bucket, key, metadata);
+                InitiateMultipartUploadResult initResponse = client.initiateMultipartUpload(initRequest);
+                uploadToken = initResponse.getUploadId();
+                logger.debug("multi-part upload initiated; token: " + uploadToken.substring(0, 16));
+            }
+            writePart();
+        }
+    }
+
+
+    private void writePart()
+    {
+        partNumber++;
+        logger.debug("uploading part " + partNumber + "; token: " + uploadToken.substring(0, 16));
+        UploadPartRequest request = new UploadPartRequest()
+                                    .withBucketName(bucket)
+                                    .withKey(key)
+                                    .withUploadId(uploadToken)
+                                    .withPartNumber(partNumber)
+                                    .withPartSize(Math.min(partSize, buffer.size()))
+                                    .withInputStream(buffer.asInputStream());
+        UploadPartResult response = client.uploadPart(request);
+        partTags.add(response.getPartETag());
+        buffer.reset();
+    }
+}
